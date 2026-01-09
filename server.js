7e7cb6c4e38c58d0fa7f05fc184aba0a530b3d9c
ShymeetriaJ <sjeducates2@gmail.com>
2026-01-09 00:31:30 -0500

    added more styles.

--- server.js
+++ server.js
@@ -1,12 +1,11 @@
 const express = require('express');
-
 const path = require('path');
 
 const app = express();
-
 const port = 3000;
 
-//route handler
+app.use(express.static(path.join(__dirname, 'public')));
+
 app.get('/', (req, res) => {
     res.sendFile(path.join(__dirname, 'public/index.html'));
 });
@@ -16,5 +15,5 @@ app.get('/contact', (req, res) => {
 });
 
 app.listen(port, () => {
-    console.log(`Server is running at http://localhost:${port}`);
-})
+    console.log(`Server is running on port ${port}`);
+});
